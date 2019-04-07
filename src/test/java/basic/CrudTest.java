a9d9a222f1a14e70ee6110c4a1e94ebad57772c6
apple <wow@github.com>
2019-04-07 10:12:16 +0800

    add selectList and insert example,
    if parameterType is an entity, mybatis will mapper properties of this object to each parameters

--- src/test/java/basic/CrudTest.java
+++ src/test/java/basic/CrudTest.java
@@ -6,24 +6,52 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class CrudTest {
-    @Test
-    public void selectMonkey() throws IOException {
+    private static SqlSessionFactory factory;
+
+    @BeforeClass
+    public static void init() throws IOException {
         String resource = "mybatis.xml";
         InputStream is = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sessionFactory
-                = new SqlSessionFactoryBuilder().build(is);
+        factory = new SqlSessionFactoryBuilder().build(is);
+    }
 
-        SqlSession session = sessionFactory.openSession();
+    @Test
+    public void selectOneMonkey() {
+        SqlSession session = factory.openSession();
 
         Monkey monkey = session.selectOne("selectMonkey", 1);
-
         System.out.println(JSONObject.toJSONString(monkey));
+
+        session.close();
+    }
+
+    @Test
+    public void selectListMonkey(){
+        SqlSession session = factory.openSession();
+
+        List<Monkey> monkeys = session.selectList("selectMonkey", 1);
+        System.out.println(JSONObject.toJSONString(monkeys));
+
+        session.close();
+    }
+
+    @Test
+    public void insertMonkey(){
+        SqlSession session = factory.openSession();
+
+        Monkey monkey = new Monkey();
+        monkey.setName("Sun WuKong");
+        int i = session.insert("insertMonkey", monkey);
+        session.commit();
+        System.out.println(i);
 
         session.close();
     }
